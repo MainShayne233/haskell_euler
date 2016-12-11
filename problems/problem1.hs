0603aed134ad2944d6b3a23ddba335e87f890837
MainShayne233 <shaynetremblay@hotmail.com>
2016-12-11 13:58:52 -0500

    solved problem 2

--- problems/problem1.hs
+++ problems/problem1.hs
@@ -1,8 +1,8 @@
-numbers :: [Integer]
+numbers :: [Int]
 numbers = [1..999]
 
-multiples :: [Integer]
+multiples :: [Int]
 multiples = [ x | x <- numbers, x `rem` 3 == 0 || x `rem` 5 == 0]
 
-solve :: Integer
-solve = sum multiples
+solve_problem_1 :: Int
+solve_problem_1 = sum multiples
--- problems/problem2.hs
+++ problems/problem2.hs
@@ -1,11 +1,17 @@
-fib_seq_to_nth :: Integer -> [Integer]
-fib_seq_to_nth n = fib_seq_to_nth' n  2  [1,2]
+sum_of_last_two :: [Int] -> Int -> Int
+sum_of_last_two [x,y] 2 = x + y
+sum_of_last_two list l = (list !! (l - 2)) + (list !! (l - 1))
 
-last_two_elems_sum :: [Integer] -> Integer
-last_two_elems_sum []   = 0
-last_two_elems_sum [x]  = x
-last_two_elems_sum list = (list !! length list - 2) + (list !! length list - 1)
+fib_seq_to_max :: Int -> [Int]
+fib_seq_to_max n = fib_seq_to_max' (n, 3, 2) [1,2]
 
-fib_seq_to_nth' :: Integer -> Integer -> [Integer] -> [Integer]
-fib_seq_to_nth' n count list | n < count  = list
-                             | otherwise  = fib_seq_to_nth' n (count + 1) (list ++ [last_two_elems_sum list])
+fib_seq_to_max' :: (Int, Int, Int) -> [Int] -> [Int]
+fib_seq_to_max' (n, m, l) list
+  | m > n = list
+  | otherwise = fib_seq_to_max' ( n, (sum_of_last_two (list ++ [m]) (l + 1)), (l + 1) ) (list ++ [m])
+
+even_fib_numbers :: [Int]
+even_fib_numbers = [x | x <- fib_seq_to_max 4000000, x `rem` 2 == 0]
+
+solve_problem_1 :: Int
+solve_problem_2 solve = sum even_fib_numbers

af62d8edc9828fd2b13ceeaa36af0030964e2f1f
MainShayne233 <shaynetremblay@hotmail.com>
2016-12-11 02:43:12 -0500

    working on problem 2

--- problems/problem2.hs
+++ problems/problem2.hs
@@ -1,3 +1,11 @@
-add_last_two list = list ++ [list !! length list - 1, list !! length list - 2]
+fib_seq_to_nth :: Integer -> [Integer]
+fib_seq_to_nth n = fib_seq_to_nth' n  2  [1,2]
 
-fib_sequence_to_nth n = fib_sequence_to_nth n 2 [1,2]
+last_two_elems_sum :: [Integer] -> Integer
+last_two_elems_sum []   = 0
+last_two_elems_sum [x]  = x
+last_two_elems_sum list = (list !! length list - 2) + (list !! length list - 1)
+
+fib_seq_to_nth' :: Integer -> Integer -> [Integer] -> [Integer]
+fib_seq_to_nth' n count list | n < count  = list
+                             | otherwise  = fib_seq_to_nth' n (count + 1) (list ++ [last_two_elems_sum list])

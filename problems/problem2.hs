3e1590585d6a3610a1bd335a3d238173f96e5062
MainShayne233 <shaynetremblay@hotmail.com>
2016-12-11 14:03:11 -0500

    cleaned up problem 2

--- problems/problem2.hs
+++ problems/problem2.hs
@@ -4,7 +4,7 @@ fib_seq_to_max n = fib_seq_to_max' (n, 3, 2) [1,2]
 fib_seq_to_max' :: (Int, Int, Int) -> [Int] -> [Int]
 fib_seq_to_max' (n, m, l) list
   | m > n = list
-  | otherwise = fib_seq_to_max' ( n, (m + (list !! (l - 1))), (l + 1) ) (list ++ [m])
+  | otherwise = fib_seq_to_max' (n, m + list !! (l - 1), l + 1) (list ++ [m])
 
 even_fib_numbers :: [Int]
 even_fib_numbers = [x | x <- fib_seq_to_max 4000000, x `rem` 2 == 0]
